7b701f6a32ee744d28cdcec65a8b1d3bf271adda
Richard <iwannaplaygamesjp@gmail.com>
2022-01-26 23:34:45 +0800

    added controls

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -2,18 +2,45 @@ import k from "../kaboom"
 import layout from "../levels"
 import levelConfig from "../levels/config"
 
-const Game = (levelIdx: number) => {
-    // add a sprite
-    /* k.add([
-        k.sprite("hero", { anim: "idle" }),
-        k.area(),
-        k.solid(),
-        k.origin("center"),
-        k.pos( k.width()/2, k.height()/2 ),
-        k.scale(3,3)
-    ]) */
+const SPEED = 100
 
-    k.addLevel(layout[levelIdx], levelConfig)
+const Game = (levelIdx: number) => {
+    const map = k.addLevel(layout[levelIdx], levelConfig);
+
+    // add a sprite
+    const player = k.add([
+        k.sprite("hero", { anim: "idle" }),
+        k.pos( map.getPos(2, 8) ),
+        k.area(),
+        k.body(),
+        k.origin("center"),
+        k.scale(3,3)
+    ]);
+
+    /* const dirs = {
+        "left": LEFT,
+        "right": RIGHT,
+        "up": UP,
+        "down": DOWN,
+    } */
+
+    k.onKeyDown("left", () => {
+        player.move(-SPEED, 0);
+        player.flipX(true)
+    })
+    
+    k.onKeyDown("right", () => {
+        player.move(SPEED, 0);
+        player.flipX(false);
+    })
+
+    k.onKeyRelease(["left", "right"], () => {
+        player.play("idle");
+    })
+
+    k.onKeyPress(["left", "right"], () => {
+        player.play("run")
+    })
     
 }
 
