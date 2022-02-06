c7075234824cfca7e6cdf66f1601298901ef9b1c
Richard <iwannaplaygamesjp@gmail.com>
2022-02-06 17:31:20 +0800

    use title scene

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import k from "./kaboom"
 import Win from "./scenes/win"
 import Lose from "./scenes/lose"
 import Game from "./scenes/game"
+import Title from "./scenes/title"
 
 k.loadSpriteAtlas(
     "/tilesets/1bitplatformer.png",
     "/tilesets/1bitplatformer.json"
 )
 
+k.scene('title', Title)
 k.scene('game', Game)
 k.scene('win', Win)
 k.scene('lose', Lose)
 
-k.go('game', 0)
+k.go('title')
